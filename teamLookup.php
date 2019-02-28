317ec365e146f7a57865e84f67afa473491836fd
imdunne8 <imdunne8@gmail.com>
2019-02-27 19:30:36 -0600

    - Basic functionality of this is working

---
+++ teamLookup.php
@@ -0,0 +1,12 @@
+<?php
+$db = new SQLite3('ScoutingDatabase.db') or die('0');
+$matchNumber = $_GET['matchNumber'];
+$station = $_GET['station'];
+$query = "SELECT " . $station . " from MatchSchedule WHERE MatchNum=" . $matchNumber;
+$results = $db->query($query);
+$teamNum = 0;
+while($row = $results->fetchArray()) {
+	$teamNum = $row[0];
+}
+echo json_encode($teamNum);
+?>
